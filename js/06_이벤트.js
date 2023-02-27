ef409b88eff3a9b9fdf91dcae89f0fc5799ab01c
STOPMIN <wlals0517@gmail.com>
2023-02-27 21:36:44 +0900

    2023.02.27 공부할것들

--- js/06_이벤트.js
+++ js/06_이벤트.js
@@ -91,3 +91,35 @@ box.addEventListener("mouseout", function(){
 })
 
 const box2 = document.getElementById("box2")
+
+box.addEventListener("mouseover", function(){
+    box.style.backgroundColor = "red";
+    box.style.width= "400px";
+
+})
+
+box2.addEventListener("mouseover", function(){
+    
+    if( count <= 10) {
+        this.style.backgroundColor = 'red';
+        this.style.color = 'white';
+        this.style.fontFamily = "궁서";
+    
+        if( count == 0 ) {
+            this.innerText = "사랑해";
+        } else {
+            this.innerText += "사랑해";
+        }
+
+        count++;
+
+    } else {
+        this.innerText = "그만해!!!"
+        this.style.backgroundColor="black";
+        this.style.color="white";
+        this.style.fontfamily="궁서";
+        this.style.lineHeight = "40px";
+    }
+
+
+})
